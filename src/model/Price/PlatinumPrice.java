e3f2f1f020183c712ce820f2e6dd9cb0b2abcd9e
chowweijie <chowweijie123@gmail.com>
2022-11-12 04:55:19 +0800

    Updated javadoc for PlatinumPrice and Price
    not completed

--- src/model/Price/PlatinumPrice.java
+++ src/model/Price/PlatinumPrice.java
@@ -3,18 +3,33 @@ package model.Price;
 import data.SystemSettingsDAO;
 import data.impl.SystemSettingsDaoImpl;
 
+/**
+ * Represents a movie registered in the system.
+ * Makes a new PlatinumPrice class.
+ * @version 1.0
+ * @since 06 Nov 2022
+ */
 public class PlatinumPrice extends Price {
 
+	/**
+	 * Default constructor for the PlatinumPrice class.
+	 */
     public PlatinumPrice() {
         super();
     }
 
+    /**
+     * Gets the price of the ticket.
+     */
     public double getTicketPrice() {
         SystemSettingsDAO systemSettingsDAO = new SystemSettingsDaoImpl();
         return systemSettingsDAO.getPlatinumPrice();
     }
 
     @Override
+    /**
+     * Updates the price of the ticket.
+     */
     public boolean updateTicketPrice(String price) {
         this.ticketPrice = Double.parseDouble(price);
         SystemSettingsDAO systemSettingsDAO = new SystemSettingsDaoImpl();
--- src/model/Price/Price.java
+++ src/model/Price/Price.java
@@ -1,22 +1,66 @@
 package model.Price;
 
+/**
+ * Represents a movie registered in the system.
+ * Makes a new Price class with weekend surcharge, student price discount, senior price discount, child price discount, public holiday surcharge, surcharge for 3D movies, surcharge after 6pm and price of the ticket.
+ * @version 1.0
+ * @since 06 Nov 2022
+ */
 public abstract class Price {
 
+	/**
+	 * Weekend surcharge.
+	 */
     protected String WeekendSurcharge = "4.0";
+    
+    /**
+     * Student price discount.
+     */
     protected String StudentPriceDiscount = "3.0";
+    
+    /**
+     * Senior price discount.
+     */
     protected String SeniorPriceDiscount = "2.0";
+    
+    /**
+     * Child price discount.
+     */
     protected String ChildPriceDiscount = "4.0";
+    
+    /**
+     * Public holiday surcharge.
+     */
     protected String PublicHolidaySurcharge = "5.0";
+    
+    /**
+     * Surcharge for 3D movie.
+     */
     protected String Surcharge3D = "2.0";
 
+    /**
+     * Surcharge after 6pm.
+     */
     protected String After6pmSurcharge = "2.0";
 
+    /**
+     * Price for the ticket.
+     */
     protected double ticketPrice;
 
+    /**
+     * Gets the price of the ticket.
+     * @return Price of the ticket.
+     */
     public double getTicketPrice() {
         return 0;
     }
 
+    /**
+     * Updates the price of the ticket.
+     * @param price New price of the ticket.
+     * @return false
+     */
     public boolean updateTicketPrice(String price){
         return false;
     };
